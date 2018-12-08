258bf381f01b770bb6118258444524df681b333d
Julien Sagot <julien.sagot@geneanet.org>
2018-12-08 20:44:42 +0100

    WIP gwdb3

--- lib/gwdb/gwdb.mli
+++ lib/gwdb/gwdb.mli
@@ -1,5 +1,21 @@
 (* Copyright (c) 1998-2007 INRIA *)
 
+(* FIXME
+
+   Add functions to:
+   - iter / map / fold on collections (persons, families, ...)
+   - fold until a condition is met
+   - loop writer help (next () function)
+   - filter, filter_map
+
+   And also:
+   - add an element to a family
+
+   So we can iper/ifam type can be really abstract and so we
+   do not rely on loops from [0] to [nb_ind] to loop over
+   collections.
+ *)
+
 open Adef
 
 type istr
